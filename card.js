830c127840345ed38be72c301f78da7dc41f78bc
nonakaco <miguelalejandro177@gmail.com>
2022-03-12 20:00:43 -0500

    Added functions for javascript.

--- card.js
+++ card.js
@@ -1,1 +1,21 @@
 document.getElementById("scoreBtn").disabled = true;
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
